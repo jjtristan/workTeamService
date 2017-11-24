7b141966eb6f563b6e2cb48e4f5214863a9b6718
Juan Tristan <jdjgomezt@gmail.com>
2017-11-24 16:20:42 -0600

    Se sube el proyecto del servicio de WorkTeamService

---
+++ WorkTeamServices/AppHost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.WebHost.Endpoints;
+
+namespace WorkTeamServices
+{
+    public class Global : System.Web.HttpApplication
+    {
+        public class AppHost : AppHostBase
+        {
+            public AppHost()
+                : base("WorkTeamServices REST", typeof(CustomerService).Assembly)
+            {
+
+            }
+        }
+    }
+    
+}
---
+++ WorkTeamServices/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkTeamServices
+{
+    public class CustomerService : Service
+    {
+        //public object Get(GetCustomerSevice)
+    }
+}
