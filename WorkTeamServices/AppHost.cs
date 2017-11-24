ec23191cfc8e8b62b94169f1597ad0acb6203bbe
Juan Tristan <jdjgomezt@gmail.com>
2017-11-24 16:55:22 -0600

    Se aniade las carpetas para las capas

--- WorkTeamServices/AppHost.cs
+++ WorkTeamServices/AppHost.cs
@@ -16,6 +16,18 @@ namespace WorkTeamServices
             {
 
             }
+
+            public override void Configure(Funq.Container container)
+            {
+                //register any dependencies your services use, e.g:
+                //container.Register<ICacheClient>(new MemoryCacheClient());
+            }
+        }
+
+        //Initialize your application singleton
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
         }
     }
     
