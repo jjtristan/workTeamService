3a41173886c597aaf3cdb789469a9a3f4e3ed300
Juan Tristan <jdjgomezt@gmail.com>
2017-12-01 13:39:58 -0600

    Se hecan a andar los servicios

--- WorkTeam.sql
+++ WorkTeam.sql
@@ -1,3 +1,22 @@
 CREATE DATABASE workTeam;
 
 USE workTeam;
+-- Tareas
+-- TareasPorActividad
+-- ActividadesPorProyecto
+-- TODO
+--Recursos
+
+create table To_do(
+	Tod_id int not null	identity(1,1) primary key
+);
+
+create table Actividades_Proyecto(
+	ActPro_id int not null
+);
+
+create table Recurso(
+	Rec_id int not null identity(1, 1) primary key,
+	Rec_Description varchar(100) not null,
+	Proyecto_id int not null
+);
---
+++ WorkTeamServices/Entities/To-do/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkTeamServices.Entities.To_do
+{
+    public class Todo
+    {
+    }
+}
